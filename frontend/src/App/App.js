7d03ab2876cd28072868bc13d4fb53abc99a19a2
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2024-01-08 06:16:07 -0500

    START PROJECT

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>MavLogistic</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
